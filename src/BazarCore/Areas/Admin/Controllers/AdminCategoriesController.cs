727628a397265538c14fae7eceba678fb335978f
eddybelo12@gmail.com <eddybelo12@gmail.com>
2023-03-26 20:12:07 +0100

    Add events, organizators and categories pages and controllers on backoffice.

---
+++ src/BazarCore/Areas/Admin/Controllers/AdminCategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazarCore.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    //[Route("categorias")]
+    public class AdminCategoriesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
